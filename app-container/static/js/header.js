1c4902c615c5959c4c6ac18a2ca615aae78cbbd4
Naylh <102329505+Naylh@users.noreply.github.com>
2023-04-10 16:31:07 +0200

    Delete app-container/static directory

--- app-container/static/js/header.js
+++
@@ -1,9 +0,0 @@
-var link = document.getElementById("desobfuscation_header");
-link.addEventListener("click", function() {
-    window.location.href = "/";
-});
-
-var link = document.getElementById("obfuscation_header");
-link.addEventListener("click", function() {
-    window.location.href = "/obfuscation";
-});
